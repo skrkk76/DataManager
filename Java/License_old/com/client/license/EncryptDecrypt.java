14e35cdec10a11136295cf1b851601b3d6d94328
Ravi Sadineni <skrkk76@gmail.com>
2025-12-24 14:56:09 +0530

    updated changes
    updated changes

---
+++ Java/License_old/com/client/license/EncryptDecrypt.java
@@ -0,0 +1,79 @@
+package com.client.license;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class EncryptDecrypt {
+    private String key = "~!@#$%^&*()_+|-={}[]:;'<>?,./";
+
+    public EncryptDecrypt() {
+    }
+
+    public String encrypt(String text) throws Exception {
+	if (text == null || "".equals(text.trim())) {
+	    return null;
+	}
+	return desEncryptDecrypt(Cipher.ENCRYPT_MODE, text);
+    }
+
+    public String decrypt(String text) throws Exception {
+	if (text == null || "".equals(text.trim())) {
+	    return null;
+	}
+	return desEncryptDecrypt(Cipher.DECRYPT_MODE, text);
+    }
+
+    private String desEncryptDecrypt(int mode, String s) throws Exception {
+	String sRet = null;
+
+	DESKeySpec dks = new DESKeySpec(key.getBytes());
+	SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+	SecretKey desKey = skf.generateSecret(dks);
+
+	Cipher cipher = Cipher.getInstance("DES");
+	if (mode == Cipher.ENCRYPT_MODE) {
+	    cipher.init(Cipher.ENCRYPT_MODE, desKey);
+	    byte[] encrypted = cipher.doFinal(s.getBytes());
+	    sRet = (new BASE64Encoder()).encode(encrypted);
+	} else if (mode == Cipher.DECRYPT_MODE) {
+	    cipher.init(Cipher.DECRYPT_MODE, desKey);
+	    byte[] in = (new BASE64Decoder()).decodeBuffer(s);
+	    byte[] out = cipher.doFinal(in);
+	    sRet = new String(out);
+	}
+
+	return sRet;
+    }
+
+    public String encryptWithSHA(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	MessageDigest md = MessageDigest.getInstance("SHA-1");
+	byte[] sha1hash = new byte[40];
+	md.update(text.getBytes("iso-8859-1"), 0, text.length());
+	sha1hash = md.digest();
+
+	StringBuilder buf = new StringBuilder();
+	for (int i = 0; i < sha1hash.length; i++) {
+	    int halfbyte = (sha1hash[i] >>> 4) & 0x0F;
+	    int two_halfs = 0;
+	    do {
+		if ((0 <= halfbyte) && (halfbyte <= 9)) {
+		    buf.append((char) ('0' + halfbyte));
+		} else {
+		    buf.append((char) ('a' + (halfbyte - 10)));
+		}
+		halfbyte = sha1hash[i] & 0x0F;
+	    } while (two_halfs++ < 1);
+	}
+
+	return buf.toString();
+    }
+}
---
+++ Java/License_old/com/client/license/VerifyLicense.java
@@ -0,0 +1,117 @@
+package com.client.license;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Locale;
+
+public class VerifyLicense {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+
+    public VerifyLicense() {
+	try {
+	    System.out.print("License File Path: ");
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    String sPath = br.readLine().trim();
+
+	    if ("".equals(sPath)) {
+		System.out.print("Please enter License File Path");
+		return;
+	    }
+
+	    File file = new File(sPath, "LICENSE");
+	    if (!file.exists()) {
+		System.out.print("Please enter License File Path");
+		return;
+	    }
+
+	    BufferedReader licenseFile = new BufferedReader(new FileReader(new File(sPath, "LICENSE")));
+	    String licenseKey = licenseFile.readLine();
+	    licenseFile.close();
+
+	    boolean isValid = verifyLicense(licenseKey);
+	    System.out.println(isValid ? "Valid License..." : "Invalid License...");
+
+	    EncryptDecrypt decrypt = new EncryptDecrypt();
+	    String decryptedValue = decrypt.decrypt(licenseKey);
+	    System.out.println("license Key : " + decryptedValue);
+
+	} catch (Exception e) {
+	    e.printStackTrace(System.out);
+	}
+    }
+
+    private boolean verifyLicense(String s) throws Exception {
+	try {
+	    EncryptDecrypt decrypt = new EncryptDecrypt();
+	    String decryptedValue = decrypt.decrypt(s);
+	    String[] saDecryptValues = decryptedValue.split("\\^");
+	    if (saDecryptValues.length < 4) {
+		return false;
+	    }
+
+	    System.out.println("License MAC Addr : " + saDecryptValues[0]);
+	    System.out.println("License HostName : " + saDecryptValues[1]);
+	    System.out.println("License Room Cnt : " + saDecryptValues[2]);
+	    System.out.println("License Exp Date : " + saDecryptValues[3]);
+
+	    String OS = System.getProperty("os.name").toLowerCase();
+
+	    InetAddress addr = InetAddress.getLocalHost();
+	    String sHostName = addr.getHostName();
+
+	    Date todayDate = new Date();
+	    Date expiryDate = sdf.parse(saDecryptValues[3]);
+
+	    boolean bFlag = sHostName.equals(saDecryptValues[1])
+		    && (todayDate.before(expiryDate) || todayDate.equals(expiryDate));
+
+	    if (OS.indexOf("win") >= 0) {
+		Enumeration<NetworkInterface> networks = NetworkInterface.getNetworkInterfaces();
+		while (networks.hasMoreElements()) {
+		    NetworkInterface network = networks.nextElement();
+		    byte[] mac = network.getHardwareAddress();
+
+		    if (mac != null) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < mac.length; i++) {
+			    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+			}
+			String sMacAddr = sb.toString();
+
+			System.out.println("HostName : " + sHostName);
+			System.out.println("Sys Date : " + sdf.format(todayDate));
+			System.out.println("MAC Addr : " + sMacAddr);
+
+			if (sMacAddr.equals(saDecryptValues[0]) && bFlag) {
+			    return true;
+			}
+		    }
+		}
+	    } else if (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") >= 0) {
+		Process p = Runtime.getRuntime().exec("ifconfig");
+		BufferedReader in = new BufferedReader(new java.io.InputStreamReader(p.getInputStream()));
+		String line = in.readLine().trim();
+		String sMacAddr = line.substring(line.lastIndexOf(" "));
+
+		System.out.println("HostName : " + sHostName);
+		System.out.println("Sys Date : " + sdf.format(todayDate));
+		System.out.println("MAC Addr : " + sMacAddr);
+
+		if (sMacAddr.equals(saDecryptValues[0]) && bFlag) {
+		    return true;
+		}
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace(System.out);
+	    return false;
+	}
+	return false;
+    }
+}
